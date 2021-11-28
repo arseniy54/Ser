61bda661190a30251e426431c301b7aaf78da9ef
Arseniy43 <74465712+Arseniy43@users.noreply.github.com>
2021-11-28 03:25:42 +0300

    Initial commit

---
+++ Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rubicon.Models
+{
+    public class booking
+    {
+        public int Id { get; set; }
+
+        public int Time { get; set; }
+
+        public int occude { get; set; }
+
+
+    }
+}
